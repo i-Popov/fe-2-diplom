c979d6a2f54158422d28ed916384ba3e230b3ec6
Igor Popov <ipopov@visiblemagic.com>
2022-09-08 13:09:47 +0300

    Confirmed order page

--- src/components/Confirmed/index.js
+++ src/components/Confirmed/index.js
@@ -2,88 +2,90 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { numSpc } from '../../helpers';
+import { Container } from '../UI/Container';
 import iconRuble from '../../assets/images/icon_ruble.png';
 import iconOrderTicketSend from '../../assets/images/icon_order_ticket_send.png';
 import iconOrderTicketPrint from '../../assets/images/icon_order_ticket_print.png';
 import iconOrderTicketPresent from '../../assets/images/icon_order_ticket_present.png';
 import iconRateService from '../../assets/images/icon_rate_service.png';
+import styles from './styles.module.scss';
 
 const OrderConfirmed = (props) => {
   return (
-    <div>
-      <div>
-        <div>
+    <section>
+      <div className={styles.success}>
+        <Container>
           <h2>Благодарим Вас за заказ!</h2>
-          <div>
-            <div>
+        </Container>
+      </div>
+      <div className={styles.data}>
+        <Container>
+          <div className={styles.data__row}>
+            <div className={styles.data__number}>
               <h4>№Заказа 285АА</h4>
-              <div>
-                <p>сумма</p>
-                <p>
+              <div className={styles.data__number__sum}>
+                <p className={styles.data__number__text}>сумма</p>
+                <p className={styles.data__number__total}>
                   {numSpc(props.ticketsAdult * props.payAdult + props.ticketsChild * props.payChild)}
                   <img src={iconRuble} alt="..." />
                 </p>
               </div>
             </div>
-            <div>
-              <div>
-                <div>
-                  <div>
-                    <div>
-                      <img src={iconOrderTicketSend} alt="..." />
-                    </div>
-                    <p>
-                      билеты будут
-                      <br />
-                      отправлены
-                      <br />
-                      на ваш e-mail
-                    </p>
-                  </div>
-                  <div>
-                    <div>
-                      <img src={iconOrderTicketPrint} alt="..." />
-                    </div>
-                    <p>распечатайте и сохраняйте билеты до даты поездки</p>
-                  </div>
-                  <div>
-                    <div>
-                      <img src={iconOrderTicketPresent} alt="..." />
-                    </div>
-                    <p>предьявите распечатанные билеты при посадке</p>
-                  </div>
+            <div className={styles.data__info}>
+              <div className={styles.data__info__row}>
+                <div className={styles.data__info__item}>
+                  <img src={iconOrderTicketSend} alt="..." />
+                  <p>
+                    Билеты будут отправлены на ваш <br /> <strong>e-mail</strong>
+                  </p>
+                </div>
+                <div className={styles.data__info__item}>
+                  <img src={iconOrderTicketPrint} alt="..." />
+                  <p>
+                    {' '}
+                    <strong>Распечатайте</strong> и сохраняйте билеты до даты поездки
+                  </p>
+                </div>
+                <div className={styles.data__info__item}>
+                  <img src={iconOrderTicketPresent} alt="..." />
+                  <p>
+                    <strong>Предьявите</strong> распечатанные билеты при посадке
+                  </p>
                 </div>
               </div>
             </div>
-            <div>
+            <div className={styles.data__thanks}>
               <h2>
                 {props.firstName} {props.patronymic}!
               </h2>
-              <p>Ваш заказ успешно оформлен.</p>
-              <p>В ближайшее время с вами свяжется наш оператор для подтверждения.</p>
-              <h4>Благодарим Вас за оказанное доверие и желаем приятного путешествия!</h4>
+              <p>
+                Ваш заказ успешно оформлен. <br /> В ближайшее время с вами свяжется наш оператор для подтверждения.
+              </p>
+              <p className={styles.data__thanks__text}>
+                Благодарим Вас за оказанное доверие и желаем приятного путешествия!
+              </p>
             </div>
-            <div>
-              <div>
+            <div className={styles.data__rate}>
+              <div className={styles.data__rate__star}>
                 <h4>Оценить сервис</h4>
-                <div>
+                <div className={styles.data__rate__img}>
                   <img src={iconRateService} alt="..." />
                   <img src={iconRateService} alt="..." />
                   <img src={iconRateService} alt="..." />
                   <img src={iconRateService} alt="..." />
                   <img src={iconRateService} alt="..." />
                 </div>
-                <div>
-                  <NavLink to="/" type="button">
-                    вернуться на главную
-                  </NavLink>
-                </div>
+              </div>
+              <div className={styles.data__rate__button}>
+                <NavLink to="/" type="button">
+                  вернуться на главную
+                </NavLink>
               </div>
             </div>
           </div>
-        </div>
+        </Container>
       </div>
-    </div>
+    </section>
   );
 };
 
